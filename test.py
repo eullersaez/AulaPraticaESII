25aeb571d1403703db9696e15998da9934545ae4
euller <eullersaez@gmail.com>
2022-06-16 20:37:47 -0300

    correção teste

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import main
 
 def test_convert_inches_to_centimeters():
-    assert main.convert_centimeters_to_inches(1) == 2.54
-    assert main.convert_centimeters_to_inches(0.5) == 1.27
-    assert main.convert_centimeters_to_inches(3.5) == 8.89
+    assert main.convert_inches_to_centimeters(1) == 2.54
+    assert main.convert_inches_to_centimeters(0.5) == 1.27
+    assert main.convert_inches_to_centimeters(3.5) == 8.89
 
 def test_convert_centimeters_to_inches():
     assert main.convert_centimeters_to_inches(1) == 0.39
